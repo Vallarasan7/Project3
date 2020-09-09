121dfcc34998c00a171f3e7912cdc264baac11d6
Vallarasan7 <71009567+Vallarasan7@users.noreply.github.com>
2020-09-09 15:55:50 +0530

    Add files via upload

---
+++ Project3.py
@@ -0,0 +1,15 @@
+#Assigning elements to a list.
+a=[1,2,3]
+a.append(4)
+a.append(5)
+print(a)
+#Accessing elements from a tuple.
+b=(1,2,3,4,5)
+print(b[0:3])
+print(b[3])
+print(b[3:5])
+#Deleting different dictionary elements.
+c={1:1,2:2,3:3,4:4,5:5}
+c.pop(1)
+c.pop(3)
+print(c)
